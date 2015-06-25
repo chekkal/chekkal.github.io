0a9a4a263cf603851dcfaa726d9a5c9080acad16
Samir Chekkal <samir.chekkal@nespresso.com>
2015-06-25 09:55:34 +0100

    rewrite dynamicproxy profiler using java8 lambda expression: extract the lamdba into a variable, now we can use it in multiple places ;-)

--- training/java/dynamicproxy/profiler/java8/Test.java
+++ training/java/dynamicproxy/profiler/java8/Test.java
@@ -3,12 +3,12 @@ import java.lang.reflect.Proxy;
 public class Test {
   public static void main(String[] args) {
     ITask task = new Task();
-    ITask proxy = (ITask)Proxy.newProxyInstance(Test.class.getClassLoader(), Task.class.getInterfaces(), 
-                        (_proxy, method, arguments) -> {
+    java.lang.reflect.InvocationHandler ikvHandler = (_proxy, method, arguments) -> {
 			  long start = System.currentTimeMillis();
 			  Object result = method.invoke(task, arguments);
 			  System.out.println("took " + (System.currentTimeMillis() -start) + " Millis.");
-			  return result; });
+			  return result; };
+    ITask proxy = (ITask)Proxy.newProxyInstance(Test.class.getClassLoader(), Task.class.getInterfaces(), ikvHandler);
     proxy.work(args[0], Integer.parseInt(args[1]));
   }
 } 
