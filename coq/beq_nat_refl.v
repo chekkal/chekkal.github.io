041db5c81aa4a61a1ca15b295f39ae33d739d634
chekkal <chekkal@gmail.com>
2016-04-28 00:05:11 +0100

    moved coq solutions to software foundations repository

--- coq/beq_nat_refl.v
+++
@@ -1,8 +0,0 @@
-(* Software Foundations *)
-(* Exercice ** beq_nat_refl *)
-Theorem beq_nat_refl: forall n: nat, true = Nat.eqb n n.
-Proof.
-    intros. induction n as [|n'].
-    simpl. reflexivity.
-    simpl. rewrite IHn'. reflexivity.
-Qed.
